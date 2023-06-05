0b45df211822c55dab09d93f33cf880ea15b37bf
yudistjr <wahyudistiragunners@gmail.com>
2023-06-05 15:26:04 +0700

    Fix link navbar

--- app/Views/template_siswa/navbar.php
+++ app/Views/template_siswa/navbar.php
@@ -1,16 +1,14 @@
    <!-- ======= Header ======= -->
    <header id="header" class="fixed-top">
        <div class="container d-flex align-items-center justify-content-between">
-
            <a href="" class="logo"><img src="<?= base_url() ?>/assets/logo.png" alt="" style="max-height: 60px !important;" class="img-fluid"></a>
-
            <nav id="navbar" class="navbar">
                <ul>
-                   <li><a class="nav-link scrollto active" href="<?= base_url('home/') ?>#hero">Home</a></li>
-                   <li><a class="nav-link scrollto" href="<?= base_url('home/') ?>#about">Tentang</a></li>
-                   <li><a class="nav-link scrollto" href="<?= base_url('home/') ?>#prestasi">Prestasi</a></li>
-                   <li><a class="nav-link scrollto " href="<?= base_url('home/') ?>#gallery">Galeri</a></li>
-                   <li><a class="nav-link scrollto" href="<?= base_url('home/') ?>#ekskul">Ekskul</a></li>
+                   <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+                   <li><a class="nav-link scrollto" href="#about">Tentang</a></li>
+                   <li><a class="nav-link scrollto" href="#prestasi">Prestasi</a></li>
+                   <li><a class="nav-link scrollto " href="#gallery">Galeri</a></li>
+                   <li><a class="nav-link scrollto" href="#ekskul">Ekskul</a></li>
                    <?php if (session('log_auth')) : ?>
                        <li class="nav-item dropdown">
                            <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">
@@ -40,6 +38,5 @@
                </ul>
                <i class="bi bi-list mobile-nav-toggle"></i>
            </nav><!-- .navbar -->
-
        </div>
    </header><!-- End Header -->
