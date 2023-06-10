20239415213701ce9654060c3b07812bd0cecc23
yusufseptian <yusufseptian39@gmail.com>
2023-06-10 18:04:19 +0700

    galery-ekskul

--- app/Views/template_siswa/ekskul.php
+++ app/Views/template_siswa/ekskul.php
@@ -2,9 +2,7 @@
 
     <div class="section-title">
         <h2>Ekstrakurikuler</h2>
-        <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
-            consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit
-            in iste officiis commodi quidem hic quas.</p>
+        <p>SMP Negeri 2 Lelea, memiliki beberapa ekstrakurikuler yang bagus, unggul, dan berkualitas. Dibuktikan dengan diraihnya penghargaan diberbagai bidang kejuaraan, mulai dari tinggat Kecamatan, Kabupaten, Provinsi, dan Nasional.</p>
     </div>
 
     <div class="row">
--- app/Views/template_siswa/galeri.php
+++ app/Views/template_siswa/galeri.php
@@ -2,9 +2,7 @@
 
     <div class="section-title">
         <h2>Galeri</h2>
-        <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
-            consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit
-            in iste officiis commodi quidem hic quas.</p>
+        <p>Adapun fasilitas dan kegiatan di SMP Negeri 2 Lelea dapat dilihat dibawah ini:</p>
     </div>
 
     <div class="row no-gutters">
